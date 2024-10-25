4e8e1daf183f48d314508ca100c9068d127e2d6b
GloomCraftST <148912165+GloomCraftST@users.noreply.github.com>
2024-10-24 21:34:50 -0400

    Update Badge_Giver.lua

--- assets/Badge_Giver/Badge_Giver.lua
+++ assets/Badge_Giver/Badge_Giver.lua
@@ -1,1 +1,25 @@
+return {
+    {
+        Type = "Script",
+        Properties = {
+            Source = [[
+-- Remember make a badge on Roblox Studio, and put the id in the Attribute called BadgeID in properties
+local BadgeID = nil
+local RS = game:GetService("ReplicatedStorage")
+local Event_Connection = script:GetAttribute("Event_Connection")
+local BS = game:GetService("BadgeService")
 
+local CF = RS:WaitForChild("BadgeEvent (Connection)")
+local Event = CF:WaitForChild(Event_Connection)
+
+Event.OnServerEvent:Connect(function(plr, ID)
+	BadgeID = ID
+	print(plr.Name.." has gotten this badge "..BadgeID)
+	BS:AwardBadge(plr.UserId, BadgeID)
+end)
+            ]],
+            Name = "BadgeScript",  -- Optional: name for the script
+            Enabled = true         -- Optional: whether script starts enabled
+        }
+    }
+}
