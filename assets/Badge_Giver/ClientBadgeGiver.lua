8cfafcf766b2ab34c47665d3241355b688d91875
GloomCraftST <148912165+GloomCraftST@users.noreply.github.com>
2024-10-24 21:52:50 -0400

    Update ClientBadgeGiver.lua

--- assets/Badge_Giver/ClientBadgeGiver.lua
+++ assets/Badge_Giver/ClientBadgeGiver.lua
@@ -1,1 +1,19 @@
+return {
+    {
+        Type = "LocalScript",
+        Properties = {
+            Source = [[
+local BadgeID = script:GetAttribute("BadgeID")
+local Event_Connection = script:GetAttribute("Event_Connection")
 
+local RS = game:GetService("ReplicatedStorage")
+local FC = RS:WaitForChild("BadgeEvent (Connection)")
+local Event = FC:WaitForChild(Event_Connection)
+
+Event:FireServer(BadgeID)
+            ]],
+            Name = "ClientBadgeScript",  -- Optional: name for the script
+            Enabled = true         -- Optional: whether script starts enabled
+        }
+    }
+}
