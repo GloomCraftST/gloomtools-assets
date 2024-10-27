5355540ddf51173d1dbe24b5864c3c8d180e6310
GloomCraftST <148912165+GloomCraftST@users.noreply.github.com>
2024-10-27 19:36:34 -0400

    Update JumpScript.lua

--- assets/Bounce_Part/JumpScript.lua
+++ assets/Bounce_Part/JumpScript.lua
@@ -1,3 +1,8 @@
+return {
+    {
+        Type = "Script",
+        Properties = {
+            Source = [[
 local JumpPart = script.Parent
 
 JumpPart.Touched:Connect(function(TouchedPart)
@@ -7,3 +12,9 @@ JumpPart.Touched:Connect(function(TouchedPart)
 	end
 	TouchedPart.Parent:FindFirstChildWhichIsA("Humanoid").JumpPower = 50
 end)
+            ]],
+            Name = "BadgeScript",  -- Optional: name for the script
+            Enabled = true         -- Optional: whether script starts enabled
+        }
+    }
+}
